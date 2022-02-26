e9dc2a19b03717892c7a0544e784e810a39534a5
kimsanghee2021 <tkdgmltjddnr@naver.com>
2022-02-26 09:49:21 +0900

    로딩화면현재 이미지 순번 및 전체이미지 갯수

--- sequence_ui/js/main.js
+++ sequence_ui/js/main.js
@@ -26,8 +26,16 @@ for(let i = 0; i < 200; i++){
     main.append(imgNode);
 }
 
-/* HTML dom이 모두 화면상에 렌더링 완료되었다고 하더라도 해당 DOM 요소에 수반되는 소소 이미지 영상 로딩과는 별개로 뜬다.
+/* 
+
+HTML dom이 모두 화면상에 렌더링 완료되었다고 하더라도 해당 DOM 요소에 수반되는 소소 이미지 영상 로딩과는 별개로 뜬다.
 시스템이 발생시키는 이벤트 onload,onerror
+onload = 해당 DOM에 수반되는 소스 이미지가 로딩온료시
+onerror = 해당DOM에 수반된는 소스들에 문제가 발생되어 출력되지 않을때
+onLoadeddata = 헤딩 DOM에 수반되는 video, audio 멀티미디어 소스 로딩 완료시
+
+퍼센트 구하는 공식(백분율)
+(현재값 / 전체값)*100
 */
 
 //모든 img dom 요소의 소스 이미지 로딩 완료시 메인화면 출력
@@ -36,10 +44,13 @@ const aside = document.querySelector('aside');
 const len = imgs.length;
 let total = 0;
 
+
 imgs.forEach(function(img){
     img.onload = function(){
         total++;
         console.log(total);
+        aside.innerText = `(${total} /${len}) `);
+
         if(total == len)aside.remove();
     }
 });
