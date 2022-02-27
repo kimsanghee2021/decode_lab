6e4467847f0067af88b926a8c70dddfe800bf84b
kimsanghee2021 <tkdgmltjddnr@naver.com>
2022-02-27 11:18:06 +0900

    객체형태로 옵션응ㄹ 뽑아서 조건문 반복처리

--- time_rwd/js/main.js
+++ time_rwd/js/main.js
@@ -6,45 +6,49 @@
     setInterval(실행할 함수, 반복간격) - 반복간격시간마다 함수 계속 호출
 */
 
-const time = document.querySelectorAll('.screen span');
+const numbers = document.querySelectorAll('.screen span');
 const main = document.querySelector('main');
-console.log(time);
+
 
 setInterval(function(){
+
+    //getTime 함수로 시간, 분, 초값을 배열로 반환
+    const times = getTime();
+
+    const data = [
+        {condition :times[0] >= 5 && times[0] < 11, name : 'morning' },
+        {condition :times[0] >= 11 && times[0] < 16, name : 'afternoon' },
+        {condition :times[0] >= 16 && times[0] < 20, name : 'evening' },
+        {condition :times[0] >= 20 || times[0] < 5, name : 'night' },
+    ]
+    //반환된 숫자값들을 반복을 돌며 값을 보정해서 선택자의 순번에 맞게 적용
+    //times.forEach((time, idx)=>setTime(time,idx));
+    times.forEach(function(time, index){
+        setTime(time, index);
+    });
+
+    data.forEach(function(item){
+        if(item.condition){
+            main.className = '';
+            main.classList.add(item.name);
+        }
+    });
+
+},1000);
+
+
+//시간 값을 구해서 [시간,분,초]로 반환하는 함수
+function getTime(){
     const now = new Date();
     let hr = now.getHours();
     let min = now.getMinutes();
     let sec = now.getSeconds();
-    
-    if(hr >= 5 && hr < 11){
-        main.className = '';
-        main.classList.add('morning');
-    }
-    if(hr >= 11 && hr < 16){
-        main.className = '';
-        main.classList.add('afternoon');
-    }
-    if(hr >= 16 && hr < 20){
-        main.className = '';
-        main.classList.add('evening');
-    }
-    if(hr >= 20 || hr < 5){
-        main.className = '';
-        main.classList.add('night');
-    }
+    return [hr,min,sec];
+}
 
-    if(hr < 10 ) hr = '0' + hr;
-    if(min < 10 ) min = '0' + min;
-    if(sec < 10 ) sec ='0' + sec;
-
-    time[0].innerText =hr;
-    time[1].innerText =min;
-    time[2].innerText =sec;
-
-},1000);
-
-//미션 1. 각 시간값이 한자리 일대 앞쪽에 0을 추가 해보기
-
-
-
+//변환할 숫자값과 순서값을 인수로 받아서 숫자를 보정하고 해당 인수의 부모요소에 값 적용하는 함수
+function setTime(num, index){
+    if(num < 10) num = '0' + num;
+    numbers[index].innerText = num;
+}
 
