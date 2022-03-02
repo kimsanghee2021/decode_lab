6824262024d5dfc671bccde6faced18a1bb9f321
kimsanghee2021 <tkdgmltjddnr@naver.com>
2022-03-02 21:58:31 +0900

    js 수정

--- time_rwd/js/main.js
+++ time_rwd/js/main.js
@@ -1,79 +1,59 @@
-﻿
-
-
-/*
-    setTimeout(실행할 함수, 지연시간) - 지연시간 이후 함수 실행
-    setInterval(실행할 함수, 반복간격) - 반복간격시간마다 함수 계속 호출
-*/
-
+﻿const main = document.querySelector('main');
 const numbers = document.querySelectorAll('.screen span');
-const main = document.querySelector('main');
 const menus = document.querySelectorAll('nav span');
-const [am,pm] = document.querySelectorAll('.screen em');
+const [am, pm] = document.querySelectorAll('.screen em');
 
-setInterval(function(){
+setInterval(function() {
+const times = getTime();
+const data = [
+{ condition: times[3] >= 5 && times[3] < 11, name: 'morning' },
+{ condition: times[3] >= 11 && times[3] < 16, name: 'afternoon' },
+{ condition: times[3] >= 16 && times[3] < 20, name: 'evening' },
+{ condition: times[3] >= 20 || times[3] < 5, name: 'night' },
+]
+times.forEach(function(time, idx) {
+if (idx < 3) {
+setTime(time, idx)
+}
+});
 
-    //getTime 함수로 시간, 분, 초값을 배열로 반환
-    const times = getTime();
+data.forEach(function(item, index) {
+if (item.condition) {
+main.className = '';
+main.classList.add(item.name);
+for (const menu of menus) menu.classList.remove('on');
+menus[index].classList.add('on');
+}
+});
 
-    const data = [
-        {condition :times[0] >= 5 && times[0] < 11, name : 'morning' },
-        {condition :times[0] >= 11 && times[0] < 16, name : 'afternoon' },
-        {condition :times[0] >= 16 && times[0] < 20, name : 'evening' },
-        {condition :times[0] >= 20 || times[0] < 5, name : 'night' },
-    ]
+if (main.classList.contains('afternoon')) {
+main.classList.add('dark_txt');
+} else {
+main.classList.remove('dark_txt');
+}
+}, 1000);
 
-    //반환된 숫자값들을 반복을 돌며 값을 보정해서 선택자의 순번에 맞게 적용
-    //times.forEach((time, idx)=>setTime(time,idx));
-    times.forEach(function(time, index){
-        setTime(time, index);
-    });
+function getTime() {
+const now = new Date();
+let hr = now.getHours();
+let min = now.getMinutes();
+let sec = now.getSeconds();
+let modified_hr = null;
 
-    //data의 정보값을 반복 돌면서 조건문 설정
-    data.forEach(function(item, idx){
-        if(item.condition){
-            main.className = '';
-            main.classList.add(item.name);
-
-            for(const menu of menus){
-                menu.classList.remove('on');
-                menus[idx].classList.add('on');
-            }
-        }
-    });
-
-    //현재 매인의 클래스 명이 afternoon일때만 추가로 dark_txt클래스 추가하기
-    if(main.classList.contains('afternoon')){
-        main.classList.add('dark_txt');
-    } else{
-        main.classList.remove('dark_txt');
-    }
-},1000);
-
-
-//시간 값을 구해서 [시간,분,초]로 반환하는 함수
-function getTime(){
-    const now = new Date();
-    let hr = now.getHours();
-    let min = now.getMinutes();
-    let sec = now.getSeconds();
-    let modified_hr = 0;
-
-    if(hr > 12){
-        modified_hr = hr - 12;
-        am.classList.remove('on');
-        pm.classList.add('on');
-    } else{
-        modified_hr = hr;
-        am.classList.add('on');
-        pm.classList.remove('on');
-    }
-    return [modified_hr,min,sec, hr];
+if (hr > 12) {
+modified_hr = hr - 12;
+am.classList.remove('on');
+pm.classList.add('on');
+} else {
+modified_hr = hr;
+am.classList.add('on');
+pm.classList.remove('on');
+}
+return [modified_hr, min, sec, hr];
 }
 
-//변환할 숫자값과 순서값을 인수로 받아서 숫자를 보정하고 해당 인수의 부모요소에 값 적용하는 함수
-function setTime(num, index){
-    if(num < 10) num = '0' + num;
-    numbers[index].innerText = num;
+function setTime(num, index) {
+if (num < 10) num = '0' + num;
+numbers[index].innerText = num;
 }
 
